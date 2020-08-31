b19e41abbd73725964c721d05ff4826f72d55efb
Vanessa <va.au@gmx.at>
2020-08-31 10:04:45 +0200

    rdy for review

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -2,11 +2,11 @@ public class Fizzbuzz {
     public static void main(String[] args) {
 
         for (int num = 1; num < 100; num++) {
-            if (num%3 == 0 && num%5 == 0){
+            if (num % 3 == 0 && num % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (num%3 == 0){
+            } else if (num % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (num%5 == 0){
+            } else if (num % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(num);
